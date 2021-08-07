70ffac37b53abcfda5da5336eb0d206a6e732acb
Enrique Gómez Bazúa <82075568+egbazua@users.noreply.github.com>
2021-08-06 18:46:05 -0700

    Version 1.0

---
+++ Quadratic_Equation/CuadraticEquation_Form.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Quadratic_Equation
+{
+    partial class Equation_Form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Equation_Form));
+            this.Title_Lbl = new System.Windows.Forms.Label();
+            this.Equation_Lbl = new System.Windows.Forms.Label();
+            this.Equation2_Lbl = new System.Windows.Forms.Label();
+            this.a_Lbl = new System.Windows.Forms.Label();
+            this.b_Lbl = new System.Windows.Forms.Label();
+            this.c_Lbl = new System.Windows.Forms.Label();
+            this.a_Tbx = new System.Windows.Forms.TextBox();
+            this.b_Tbx = new System.Windows.Forms.TextBox();
+            this.c_Tbx = new System.Windows.Forms.TextBox();
+            this.result1_Lbl = new System.Windows.Forms.Label();
+            this.result1a_Lbl = new System.Windows.Forms.Label();
+            this.result2_Lbl = new System.Windows.Forms.Label();
+            this.result2b_Lbl = new System.Windows.Forms.Label();
+            this.Calculate_Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title_Lbl
+            // 
+            this.Title_Lbl.AutoSize = true;
+            this.Title_Lbl.BackColor = System.Drawing.Color.Black;
+            this.Title_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title_Lbl.ForeColor = System.Drawing.Color.White;
+            this.Title_Lbl.Location = new System.Drawing.Point(68, 44);
+            this.Title_Lbl.Name = "Title_Lbl";
+            this.Title_Lbl.Size = new System.Drawing.Size(370, 50);
+            this.Title_Lbl.TabIndex = 0;
+            this.Title_Lbl.Text = "Enter the data of the coefficients that \r\naccompany the variables";
+            // 
+            // Equation_Lbl
+            // 
+            this.Equation_Lbl.AutoSize = true;
+            this.Equation_Lbl.BackColor = System.Drawing.Color.Black;
+            this.Equation_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Equation_Lbl.ForeColor = System.Drawing.Color.White;
+            this.Equation_Lbl.Location = new System.Drawing.Point(188, 110);
+            this.Equation_Lbl.Name = "Equation_Lbl";
+            this.Equation_Lbl.Size = new System.Drawing.Size(129, 25);
+            this.Equation_Lbl.TabIndex = 1;
+            this.Equation_Lbl.Text = "ax+bx+c = 0";
+            // 
+            // Equation2_Lbl
+            // 
+            this.Equation2_Lbl.AutoSize = true;
+            this.Equation2_Lbl.BackColor = System.Drawing.Color.Black;
+            this.Equation2_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Equation2_Lbl.ForeColor = System.Drawing.Color.White;
+            this.Equation2_Lbl.Location = new System.Drawing.Point(211, 103);
+            this.Equation2_Lbl.Name = "Equation2_Lbl";
+            this.Equation2_Lbl.Size = new System.Drawing.Size(15, 16);
+            this.Equation2_Lbl.TabIndex = 2;
+            this.Equation2_Lbl.Text = "2";
+            // 
+            // a_Lbl
+            // 
+            this.a_Lbl.AutoSize = true;
+            this.a_Lbl.BackColor = System.Drawing.Color.Black;
+            this.a_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a_Lbl.ForeColor = System.Drawing.Color.White;
+            this.a_Lbl.Location = new System.Drawing.Point(68, 159);
+            this.a_Lbl.Name = "a_Lbl";
+            this.a_Lbl.Size = new System.Drawing.Size(48, 25);
+            this.a_Lbl.TabIndex = 3;
+            this.a_Lbl.Text = "a = ";
+            // 
+            // b_Lbl
+            // 
+            this.b_Lbl.AutoSize = true;
+            this.b_Lbl.BackColor = System.Drawing.Color.Black;
+            this.b_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_Lbl.ForeColor = System.Drawing.Color.White;
+            this.b_Lbl.Location = new System.Drawing.Point(68, 196);
+            this.b_Lbl.Name = "b_Lbl";
+            this.b_Lbl.Size = new System.Drawing.Size(48, 25);
+            this.b_Lbl.TabIndex = 4;
+            this.b_Lbl.Text = "b = ";
+            // 
+            // c_Lbl
+            // 
+            this.c_Lbl.AutoSize = true;
+            this.c_Lbl.BackColor = System.Drawing.Color.Black;
+            this.c_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c_Lbl.ForeColor = System.Drawing.Color.White;
+            this.c_Lbl.Location = new System.Drawing.Point(68, 233);
+            this.c_Lbl.Name = "c_Lbl";
+            this.c_Lbl.Size = new System.Drawing.Size(47, 25);
+            this.c_Lbl.TabIndex = 5;
+            this.c_Lbl.Text = "c = ";
+            // 
+            // a_Tbx
+            // 
+            this.a_Tbx.Location = new System.Drawing.Point(109, 162);
+            this.a_Tbx.Name = "a_Tbx";
+            this.a_Tbx.Size = new System.Drawing.Size(30, 20);
+            this.a_Tbx.TabIndex = 6;
+            // 
+            // b_Tbx
+            // 
+            this.b_Tbx.Location = new System.Drawing.Point(109, 199);
+            this.b_Tbx.Name = "b_Tbx";
+            this.b_Tbx.Size = new System.Drawing.Size(30, 20);
+            this.b_Tbx.TabIndex = 7;
+            // 
+            // c_Tbx
+            // 
+            this.c_Tbx.Location = new System.Drawing.Point(108, 236);
+            this.c_Tbx.Name = "c_Tbx";
+            this.c_Tbx.Size = new System.Drawing.Size(30, 20);
+            this.c_Tbx.TabIndex = 8;
+            // 
+            // result1_Lbl
+            // 
+            this.result1_Lbl.AutoSize = true;
+            this.result1_Lbl.BackColor = System.Drawing.Color.Black;
+            this.result1_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result1_Lbl.ForeColor = System.Drawing.Color.White;
+            this.result1_Lbl.Location = new System.Drawing.Point(189, 158);
+            this.result1_Lbl.Name = "result1_Lbl";
+            this.result1_Lbl.Size = new System.Drawing.Size(107, 25);
+            this.result1_Lbl.TabIndex = 9;
+            this.result1_Lbl.Text = "result 1 = ";
+            // 
+            // result1a_Lbl
+            // 
+            this.result1a_Lbl.AutoSize = true;
+            this.result1a_Lbl.BackColor = System.Drawing.Color.Black;
+            this.result1a_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result1a_Lbl.ForeColor = System.Drawing.Color.White;
+            this.result1a_Lbl.Location = new System.Drawing.Point(293, 158);
+            this.result1a_Lbl.Name = "result1a_Lbl";
+            this.result1a_Lbl.Size = new System.Drawing.Size(26, 25);
+            this.result1a_Lbl.TabIndex = 10;
+            this.result1a_Lbl.Text = "X";
+            // 
+            // result2_Lbl
+            // 
+            this.result2_Lbl.AutoSize = true;
+            this.result2_Lbl.BackColor = System.Drawing.Color.Black;
+            this.result2_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result2_Lbl.ForeColor = System.Drawing.Color.White;
+            this.result2_Lbl.Location = new System.Drawing.Point(189, 205);
+            this.result2_Lbl.Name = "result2_Lbl";
+            this.result2_Lbl.Size = new System.Drawing.Size(107, 25);
+            this.result2_Lbl.TabIndex = 11;
+            this.result2_Lbl.Text = "result 2 = ";
+            // 
+            // result2b_Lbl
+            // 
+            this.result2b_Lbl.AutoSize = true;
+            this.result2b_Lbl.BackColor = System.Drawing.Color.Black;
+            this.result2b_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result2b_Lbl.ForeColor = System.Drawing.Color.White;
+            this.result2b_Lbl.Location = new System.Drawing.Point(293, 205);
+            this.result2b_Lbl.Name = "result2b_Lbl";
+            this.result2b_Lbl.Size = new System.Drawing.Size(26, 25);
+            this.result2b_Lbl.TabIndex = 12;
+            this.result2b_Lbl.Text = "X";
+            // 
+            // Calculate_Btn
+            // 
+            this.Calculate_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Calculate_Btn.Location = new System.Drawing.Point(363, 237);
+            this.Calculate_Btn.Name = "Calculate_Btn";
+            this.Calculate_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Calculate_Btn.TabIndex = 13;
+            this.Calculate_Btn.Text = "Calculate";
+            this.Calculate_Btn.UseVisualStyleBackColor = true;
+            this.Calculate_Btn.Click += new System.EventHandler(this.Calculate_Btn_Click);
+            // 
+            // Equation_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(506, 286);
+            this.Controls.Add(this.Calculate_Btn);
+            this.Controls.Add(this.result2b_Lbl);
+            this.Controls.Add(this.result2_Lbl);
+            this.Controls.Add(this.result1a_Lbl);
+            this.Controls.Add(this.result1_Lbl);
+            this.Controls.Add(this.c_Tbx);
+            this.Controls.Add(this.b_Tbx);
+            this.Controls.Add(this.a_Tbx);
+            this.Controls.Add(this.c_Lbl);
+            this.Controls.Add(this.b_Lbl);
+            this.Controls.Add(this.a_Lbl);
+            this.Controls.Add(this.Equation2_Lbl);
+            this.Controls.Add(this.Equation_Lbl);
+            this.Controls.Add(this.Title_Lbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Equation_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cuadratic Equation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title_Lbl;
+        private System.Windows.Forms.Label Equation_Lbl;
+        private System.Windows.Forms.Label Equation2_Lbl;
+        private System.Windows.Forms.Label a_Lbl;
+        private System.Windows.Forms.Label b_Lbl;
+        private System.Windows.Forms.Label c_Lbl;
+        private System.Windows.Forms.TextBox a_Tbx;
+        private System.Windows.Forms.TextBox b_Tbx;
+        private System.Windows.Forms.TextBox c_Tbx;
+        private System.Windows.Forms.Label result1_Lbl;
+        private System.Windows.Forms.Label result1a_Lbl;
+        private System.Windows.Forms.Label result2_Lbl;
+        private System.Windows.Forms.Label result2b_Lbl;
+        private System.Windows.Forms.Button Calculate_Btn;
+    }
+}
+
---
+++ Quadratic_Equation/CuadraticEquation_Form.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quadratic_Equation
+{
+    public partial class Equation_Form : Form
+    {
+        double result1_r;
+        double result2_r;
+        public Equation_Form()
+        {
+            InitializeComponent();
+            result1_r=0;
+            result2_r=0;
+        }
+
+        private void Calculate_Btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(a_Tbx.Text) || string.IsNullOrEmpty(b_Tbx.Text) || string.IsNullOrEmpty(c_Tbx.Text))
+                MessageBox.Show("Favor de llenar todos los campos.");
+            else
+            {
+                result1_r = result1(Convert.ToDouble(a_Tbx.Text), Convert.ToDouble(b_Tbx.Text), Convert.ToDouble(c_Tbx.Text));
+                result2_r = result2(Convert.ToDouble(a_Tbx.Text), Convert.ToDouble(b_Tbx.Text), Convert.ToDouble(c_Tbx.Text));
+
+                result1a_Lbl.Text = Convert.ToString(result1_r);
+                result2b_Lbl.Text = Convert.ToString(result2_r);
+            }
+        }//Calculate Button.
+
+        private double result1(Double xa, Double xb, Double xc)
+        {
+            double result_r;
+
+            result_r = (-(xb) + Math.Sqrt((xb * xb) - 4 * xa * xc)) / (2 * xa);
+
+            return result_r;
+        }//result 1 method.
+        private double result2(Double xa, Double xb, Double xc)
+        {
+            double result_r;
+
+            result_r = (-(xb) - Math.Sqrt((xb * xb) - 4 * xa * xc)) / (2 * xa);
+
+            return result_r;
+        }//result 2 method.
+    }//End of class.
+}
